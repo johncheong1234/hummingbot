bf297f0e56b5c8fba3504cf6547dfcc79a4901fc
John Cheong <john.cheong@mili.eu>
2022-12-15 21:36:49 +0800

    initial perps commit

--- scripts/simple_pmm_example.py
+++ scripts/simple_pmm_example.py
@@ -3,7 +3,7 @@ from decimal import Decimal
 from typing import List
 
 from hummingbot.core.data_type.common import OrderType, PriceType, TradeType
-from hummingbot.core.data_type.order_candidate import OrderCandidate
+from hummingbot.core.data_type.order_candidate import OrderCandidate, PerpetualOrderCandidate
 from hummingbot.core.event.events import OrderFilledEvent
 from hummingbot.strategy.script_strategy_base import ScriptStrategyBase
 
@@ -15,30 +15,63 @@ class SimplePMM(ScriptStrategyBase):
     order_amount = 0.01
     create_timestamp = 0
     trading_pair = "ETH-USDT"
-    exchange = "bitmex_testnet"
+    exchange = "bitmex_perpetual_testnet"
     # Here you can use for example the LastTrade price to use in your strategy
     price_source = PriceType.MidPrice
 
     markets = {exchange: {trading_pair}}
 
     def on_tick(self):
+        orderBook = self.connectors[self.exchange].get_order_book(self.trading_pair)
+        # print snapshot of orderBook
+        self.logger().info(f"OrderBook: {orderBook}")
+        # ask_entries
+        # ask_entries = orderBook.ask_entries()
+        # print ask_entries
+        # for ask_entry in ask_entries:
+        #     self.logger().info(f"Ask Entry: {ask_entry}")
+
+        midPrice = self.connectors[self.exchange].get_mid_price(self.trading_pair)
+        self.logger().info(f"MidPrice: {midPrice}")
+
+        accountPosition = self.connectors[self.exchange].account_positions
+        self.logger().info(f"Account Position: {accountPosition}")
+
         if self.create_timestamp <= self.current_timestamp:
             self.cancel_all_orders()
-            proposal: List[OrderCandidate] = self.create_proposal()
-            proposal_adjusted: List[OrderCandidate] = self.adjust_proposal_to_budget(proposal)
-            self.place_orders(proposal_adjusted)
-            self.create_timestamp = self.order_refresh_time + self.current_timestamp
+            # proposal: List[OrderCandidate] = self.create_proposal()
+            # proposal_adjusted: List[OrderCandidate] = self.adjust_proposal_to_budget(proposal)
+            # self.place_orders(proposal_adjusted)
+            # self.create_timestamp = self.order_refresh_time + self.current_timestamp
 
     def create_proposal(self) -> List[OrderCandidate]:
         ref_price = self.connectors[self.exchange].get_price_by_type(self.trading_pair, self.price_source)
-        buy_price = ref_price * Decimal(1 - self.bid_spread)
+        # buy_price = ref_price * Decimal(1 - self.bid_spread)
         sell_price = ref_price * Decimal(1 + self.ask_spread)
 
-        buy_order = OrderCandidate(trading_pair=self.trading_pair, is_maker=True, order_type=OrderType.LIMIT,
-                                   order_side=TradeType.BUY, amount=Decimal(self.order_amount), price=buy_price)
+        buy_order = PerpetualOrderCandidate(
+            trading_pair=self.trading_pair,
+            is_maker=True,
+            order_type=OrderType.LIMIT,
+            order_side=TradeType.BUY,
+            amount=Decimal(self.order_amount),
+            price=Decimal(1300),
+            from_total_balances=True,
+            leverage=Decimal(1),
+            position_close=False,
+        )
 
-        sell_order = OrderCandidate(trading_pair=self.trading_pair, is_maker=True, order_type=OrderType.LIMIT,
-                                    order_side=TradeType.SELL, amount=Decimal(self.order_amount), price=sell_price)
+        sell_order = PerpetualOrderCandidate(
+            trading_pair=self.trading_pair,
+            is_maker=True,
+            order_type=OrderType.LIMIT,
+            order_side=TradeType.SELL,
+            amount=Decimal(self.order_amount),
+            price=sell_price,
+            from_total_balances=True,
+            leverage=Decimal(1),
+            position_close=False,
+        )
 
         return [buy_order, sell_order]
 
@@ -52,17 +85,27 @@ class SimplePMM(ScriptStrategyBase):
 
     def place_order(self, connector_name: str, order: OrderCandidate):
         if order.order_side == TradeType.SELL:
-            self.sell(connector_name=connector_name, trading_pair=order.trading_pair, amount=order.amount,
-                      order_type=order.order_type, price=order.price)
+            self.sell(
+                connector_name=connector_name,
+                trading_pair=order.trading_pair,
+                amount=order.amount,
+                order_type=order.order_type,
+                price=order.price,
+            )
         elif order.order_side == TradeType.BUY:
-            self.buy(connector_name=connector_name, trading_pair=order.trading_pair, amount=order.amount,
-                     order_type=order.order_type, price=order.price)
+            self.buy(
+                connector_name=connector_name,
+                trading_pair=order.trading_pair,
+                amount=order.amount,
+                order_type=order.order_type,
+                price=order.price,
+            )
 
     def cancel_all_orders(self):
         for order in self.get_active_orders(connector_name=self.exchange):
             self.cancel(self.exchange, order.trading_pair, order.client_order_id)
 
     def did_fill_order(self, event: OrderFilledEvent):
-        msg = (f"{event.trade_type.name} {round(event.amount, 2)} {event.trading_pair} {self.exchange} at {round(event.price, 2)}")
+        msg = f"{event.trade_type.name} {round(event.amount, 2)} {event.trading_pair} {self.exchange} at {round(event.price, 2)}"
         self.log_with_clock(logging.INFO, msg)
         self.notify_hb_app_with_timestamp(msg)
