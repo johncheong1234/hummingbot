cfc4afae8d868d775d4e471275f6078f89cb508f
johncheong1234 <john.cheong.2019@smu.edu.sg>
2022-12-24 12:38:16 +0800

    more changes

--- scripts/simple_pmm_example.py
+++ scripts/simple_pmm_example.py
@@ -12,9 +12,9 @@ class SimplePMM(ScriptStrategyBase):
     bid_spread = 0.08
     ask_spread = 0.08
     order_refresh_time = 15
-    order_amount = 0.01
+    order_amount = 100
     create_timestamp = 0
-    trading_pair = "ETH-USDT"
+    trading_pair = "LTC-USDT"
     exchange = "binance_perpetual_testnet"
     # Here you can use for example the LastTrade price to use in your strategy
     price_source = PriceType.MidPrice
@@ -31,23 +31,40 @@ class SimplePMM(ScriptStrategyBase):
         # for ask_entry in ask_entries:
         #     self.logger().info(f"Ask Entry: {ask_entry}")
 
+        buyPrice = self.connectors[self.exchange].get_price(self.trading_pair, is_buy = True)
+        sellPrice = self.connectors[self.exchange].get_price(self.trading_pair, is_buy = False)
         midPrice = self.connectors[self.exchange].get_mid_price(self.trading_pair)
-        self.logger().info(f"MidPrice: {midPrice}")
+        self.logger().info(f"buyPrice: {buyPrice}")
+        self.logger().info(f"sellPrice: {sellPrice}")
+        self.logger().info(f"midPrice: {midPrice}")
 
         accountPosition = self.connectors[self.exchange].account_positions
-        self.logger().info(f"Account Position: {accountPosition}")
+        # self.logger().info(f"Account Position: {accountPosition}")
 
-        # if self.create_timestamp <= self.current_timestamp:
-        #     self.cancel_all_orders()
-        #     proposal: List[OrderCandidate] = self.create_proposal()
-        #     proposal_adjusted: List[OrderCandidate] = self.adjust_proposal_to_budget(proposal)
-        #     self.place_orders(proposal_adjusted)
-        #     self.create_timestamp = self.order_refresh_time + self.current_timestamp
+        # iterate through dictionary accountPosition and print key and value
+        amount = 0
+        unrealized_pnl = 0
+        for key, value in accountPosition.items():
+            trading_pair = value.trading_pair
+            if (trading_pair == self.trading_pair):
+                amount = value.amount
+                unrealized_pnl = value.unrealized_pnl
+                self.logger().info(f"Key: {key}, Value: {value}, type: {type(value)}")
+
+        self.logger().info(f"Amount: {amount}, type: {type(amount)}")
+        self.logger().info(f"Unrealized PnL: {unrealized_pnl}, type: {type(unrealized_pnl)}")
+
+        if self.create_timestamp <= self.current_timestamp and amount == 0:
+            self.cancel_all_orders()
+            proposal: List[OrderCandidate] = self.create_proposal()
+            proposal_adjusted: List[OrderCandidate] = self.adjust_proposal_to_budget(proposal)
+            self.place_orders(proposal_adjusted)
+            self.create_timestamp = self.order_refresh_time + self.current_timestamp
 
     def create_proposal(self) -> List[OrderCandidate]:
-        ref_price = self.connectors[self.exchange].get_price_by_type(self.trading_pair, self.price_source)
+        # ref_price = self.connectors[self.exchange].get_price_by_type(self.trading_pair, self.price_source)
         # buy_price = ref_price * Decimal(1 - self.bid_spread)
-        sell_price = ref_price * Decimal(1 + self.ask_spread)
+        # sell_price = ref_price * Decimal(1 + self.ask_spread)
 
         buy_order = PerpetualOrderCandidate(
             trading_pair=self.trading_pair,
@@ -55,25 +72,27 @@ class SimplePMM(ScriptStrategyBase):
             order_type=OrderType.LIMIT,
             order_side=TradeType.BUY,
             amount=Decimal(self.order_amount),
-            price=Decimal(1300),
+            price=Decimal(70),
             from_total_balances=True,
             leverage=Decimal(1),
             position_close=False,
         )
 
-        sell_order = PerpetualOrderCandidate(
-            trading_pair=self.trading_pair,
-            is_maker=True,
-            order_type=OrderType.LIMIT,
-            order_side=TradeType.SELL,
-            amount=Decimal(self.order_amount),
-            price=sell_price,
-            from_total_balances=True,
-            leverage=Decimal(1),
-            position_close=False,
-        )
+        return [buy_order]
 
-        return [buy_order, sell_order]
+        # sell_order = PerpetualOrderCandidate(
+        #     trading_pair=self.trading_pair,
+        #     is_maker=True,
+        #     order_type=OrderType.LIMIT,
+        #     order_side=TradeType.SELL,
+        #     amount=Decimal(self.order_amount),
+        #     price=sell_price,
+        #     from_total_balances=True,
+        #     leverage=Decimal(1),
+        #     position_close=False,
+        # )
+
+        # return [buy_order, sell_order]
 
     def adjust_proposal_to_budget(self, proposal: List[OrderCandidate]) -> List[OrderCandidate]:
         proposal_adjusted = self.connectors[self.exchange].budget_checker.adjust_candidates(proposal, all_or_none=True)
